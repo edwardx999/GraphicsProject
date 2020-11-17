60a6136bba5bd5e26af7d67a761b2990f3237730
matttodd <matttodd6@gmail.com>
2020-11-16 23:54:43 -0500

    :)

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
 app.use(express.static('public'));
-app.listen(3000);
+app.listen(80);
 //# sourceMappingURL=server.js.map
