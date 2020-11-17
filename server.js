131a2010c1947133678197541ab0a3abbf9507a1
The_Number_e <edwardx999@gmail.com>
2020-11-17 13:29:38 -0500

    process env port

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
 app.use(express.static('public'));
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 //# sourceMappingURL=server.js.map
