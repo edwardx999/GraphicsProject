55d5aceac6de0977488162e3291565f810cfe987
matttodd <matttodd6@gmail.com>
2020-11-16 23:29:25 -0500

    ready for demo 1 :)

--- public/source.ts
+++ public/source.ts
@@ -1,7 +1,7 @@
 import * as THREE from "./lib/Three.js"
-import { SpotLight } from "./lib/Three.js";
+import { BooleanKeyframeTrack, CameraHelper, RGBA_PVRTC_2BPPV1_Format, SpotLight } from "./lib/Three.js";
 
-let camera: THREE.Camera;
+let camera: THREE.PerspectiveCamera;
 let light: THREE.SpotLight;
 let scene: THREE.Scene;
 let renderer: THREE.WebGLRenderer;
@@ -9,13 +9,15 @@ let geometry: THREE.Geometry;
 let material: THREE.Material;
 let cube: THREE.Mesh;
 let cube2: THREE.Mesh;
-let direction = [0, 0, 0];
+let direction = new THREE.Vector3(0, 0, 0);
+let theta = 0
 
 
 function init() {
 
     camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 100);
     camera.position.z = 1;
+    document.getElementById("fov").setAttribute("value", "70");
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xcce0ff);
@@ -28,7 +30,7 @@ function init() {
     cube.receiveShadow = true;
     scene.add(cube);
 
-    cube2 = new THREE.Mesh(new THREE.BoxGeometry(0.5, 0.5, 0.5), material);
+    cube2 = new THREE.Mesh(new THREE.ConeGeometry(0.5, 1, 10), material);
     cube2.castShadow = true;
     cube2.receiveShadow = true;
     cube2.position.z = -1;
@@ -76,9 +78,8 @@ const animate: FrameRequestCallback = (time) => {
     requestAnimationFrame(animate);
     cube.rotation.x += 0.001 * elapsed;
     cube.rotation.y += 0.002 * elapsed;
-    camera.position.x += direction[0] * elapsed;
-    camera.position.y += direction[1] * elapsed;
-    camera.position.z += direction[2] * elapsed;
+    camera.position.addScaledVector(direction, elapsed);
+    camera.rotation.y += theta * elapsed;
     light.position.copy(camera.position);
     light.position.y += 1;
     renderer.render(scene, camera);
@@ -88,18 +89,26 @@ init();
 requestAnimationFrame(animate);
 
 window.addEventListener("keydown", (ev) => {
+    const facing = new THREE.Vector3()
+    camera.getWorldDirection(facing)
     switch (ev.key) {
         case "w":
-            direction = [0, 0, -0.005];
+            direction.copy(facing).multiplyScalar(0.005);
             break;
         case "s":
-            direction = [0, 0, 0.005];
+            direction.copy(facing).multiplyScalar(-0.005);
             break;
         case "a":
-            direction = [-0.005, 0, 0];
+            direction.set(-facing.z, 0, facing.x).multiplyScalar(-0.005);
             break;
         case "d":
-            direction = [0.005, 0, 0];
+            direction.set(-facing.z, 0, facing.x).multiplyScalar(0.005);
+            break;
+        case "q":
+            theta = 0.001;
+            break;
+        case "e":
+            theta = -0.001;
             break;
         default:
             return;
@@ -112,7 +121,20 @@ window.addEventListener("keyup", (ev) => {
         case "s":
         case "a":
         case "d":
-            direction = [0, 0, 0];
+            direction.set(0, 0, 0);
             break;
+        case "q":
+        case "e":
+            theta = 0
+            
     }
 })
+
+const setFov = (newFov: number) => {
+    camera.fov = newFov;
+    camera.updateProjectionMatrix();
+} 
+
+document.getElementById("fov").addEventListener("change", (val) => {
+    setFov((val.target as HTMLInputElement).valueAsNumber);
+});
